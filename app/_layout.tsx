9eed3e64820991eec86b53d01f85af7a201e6e95
sams <djoudersamy@gmail.com>
2026-06-07 19:52:53 +0200

    removed hardcoded spacemono font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import {
   PathwayExtreme_400Regular,
   PathwayExtreme_400Regular_Italic,
@@ -9,11 +8,12 @@ import {
   PathwayExtreme_800ExtraBold,
   PathwayExtreme_900Black,
 } from '@expo-google-fonts/pathway-extreme';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'react-native-reanimated';
 import '../global.css';
 
@@ -22,7 +22,7 @@ import { FONT_SEMIBOLD } from '@/constants/fonts';
 
 export {
   // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+  ErrorBoundary
 } from 'expo-router';
 
 export const unstable_settings = {
@@ -35,7 +35,6 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     PathwayExtreme_400Regular,
     PathwayExtreme_400Regular_Italic,
     PathwayExtreme_500Medium,
